ad57f895a31e1c205d8d5165fc985ce1c12f4e25
Heros <herosrac@gmail.com>
2021-07-21 12:45:26 -0300

    Adding the first C++ exercise on Git

---
+++ Working_with_arrays.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <limits>
+
+void print_array(int array[], int size)
+{
+    for(int i = 0; i < size; i++)
+        std::cout << array[i] << "\t";
+}
+
+
+int main()
+{
+    const int SIZE = 10;
+    int guesses[SIZE];    
+
+    int count = 0;
+
+    for(int i = 0; i < SIZE; i++)
+    {
+        std::cout << "Please, input the " << i << "o number: ";
+        if(std::cin >> guesses[i])
+        {
+            count++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    print_array(guesses, count);
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::string test;
+    std::cin >> test;
+    std::cout << test;
+}
