4f2155fa7627f65aa19dd9d9eceeaf262cb27f43
Heros <herosrac@gmail.com>
2021-07-23 23:04:26 -0300

    Inserting unique values in a vector + While loop (I still have to finish)

--- Exercises/random_numbers.cpp
+++ Exercises/random_numbers.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <vector>
 #include <ctime>
 #include <cstdlib>
 
 
-int main() 
+void biggest_smallest(std::vector<int> random_storage)
 {
     int biggest {0};
     int smallest {0};
+
+    for(auto i = 0; i < random_storage.size(); i++)
+    {
+        std::cout << random_storage[i] << " ";
+        if(i == 0)
+        {
+            biggest = random_storage[i];
+            smallest = random_storage[i];
+        }
+        else
+        {
+            if(random_storage[i] > biggest)
+            {
+                biggest = random_storage[i];
+            }
+            if(random_storage[i] < smallest)
+            {
+                smallest = random_storage[i];
+            }
+        }
+    }
+
+    std::cout << "\nThe biggest random number generated is: " << biggest << std::endl; 
+    std::cout << "The smallest random number generated is: " << smallest << std::endl;
+}
+
+
+int main() 
+{
+    std::vector<int> random_storage;
 
     srand(time(0));  // Initialize random number generator.     
     std::cout << "The random numbers generated between 1 and 10 are: ";
@@ -14,24 +45,8 @@ int main()
     for(int i = 0; i < 5; i++)
     {
         int random_number = (rand() % 10) + 1;
-        std::cout << random_number << " "; 
-        if(i == 0)
-        {
-            biggest = random_number;
-            smallest = random_number;
-        }
-        else
-        {
-            if(random_number > biggest)
-            {
-                biggest = random_number;
-            }
-            if(random_number < smallest)
-            {
-                smallest = random_number;
-            }
-        }
+        random_storage.push_back(random_number);        
     }
-    std::cout << "\nThe biggest random number generated is: " << biggest << std::endl; 
-    std::cout << "The smallest random number generated is: " << smallest << std::endl;
+
+    biggest_smallest(random_storage);
 }
