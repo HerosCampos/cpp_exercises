bc4cd9c3d53234b72f1e23a835d50dbfc22d724c
CodeCoin <herosrac@gmail.com>
2021-08-08 18:30:06 -0300

    adding Products Class that will store values in a vector

--- Project_1_TechStore/oop_products.cpp
+++ Project_1_TechStore/oop_products.cpp
@@ -3,49 +3,53 @@
 #include <string>
 
 
-class Products
+class Product
 {
 private:
+    int product_id;
     std::string name;
     std::string brand;
     double price;
     int quantity;
 
 public:
+    int get_product_id() {return product_id;}
     std::string get_name() {return name;}
     std::string get_brand() {return brand;}
     double get_price() {return price;}
     int get_quantity() {return quantity;}
 
+    void set_product_id(int product_id_val) {product_id = product_id_val;}
     void set_name(std::string name_val) {name = name_val;}
     void set_brand(std::string brand_val) {brand = brand_val;}
     void set_price(double price_val) {price = price_val;}
     void set_quantity(int quantity_val) {quantity = quantity_val;}
 
-    Products(std::string name_val = "No_Name", std::string brand_val = "No_brand", double price_val = 0.0, int quantity_val = 0);
+    Product(int product_id_val = 0000, std::string name_val = "No_Name", std::string brand_val = "No_brand", double price_val = 0.0, int quantity_val = 0);
 
-    Products(const Products &source);
+    Product(const Product &source);
 
-    ~Products()
+    ~Product()
     {
         std::cout << "Calling destructor for: " << name << std::endl;
     }
 };
 
 
-Products::Products(std::string name_val, std::string brand_val, double price_val, int quantity_val)
-    :name{name_val}, brand{brand_val}, price{price_val}, quantity{quantity_val} {
+Product::Product(int product_id_val, std::string name_val, std::string brand_val, double price_val, int quantity_val)
+    :product_id{product_id_val}, name{name_val}, brand{brand_val}, price{price_val}, quantity{quantity_val} {
         std::cout << "Major constructor called for: " << name << std::endl;
 }
 
-Products::Products(const Products &source)
-    :Products{source.name, source.brand, source.price, source.quantity} {
+Product::Product(const Product &source)
+    :Product{source.product_id, source.name, source.brand, source.price, source.quantity} {
         std::cout << "Copy constructor called for: " << source.name << std::endl;
 }
 
 
-void display_product(Products p)
+void display_product(Product p)
 {
+    std::cout << "Product ID: " << p.get_product_id() << std::endl;
     std::cout << "Name: " << p.get_name() << std::endl;
     std::cout << "Brand: " << p.get_brand() << std::endl;
     std::cout << "Price: " << p.get_price() << std::endl;
@@ -53,11 +57,29 @@ void display_product(Products p)
 }
 
 
+
+
+
+
+class Products
+{
+private:
+    std::vector<Product> all_products;
+
+public:
+    
+};
+
+
+
+
+
+
 int main()
 {
-    Products teclado("Teclado Premium", "Microsoft", 89.90, 8);
-    Products mouse("Mouse sem fio", "Microsoft", 22.49, 4);
-    Products mouse_pad("Mouse Pad", "Lutier");
+    Product teclado(0001, "Teclado Premium", "Microsoft", 89.90, 8);
+    Product mouse(0002, "Mouse sem fio", "Microsoft", 22.49, 4);
+    Product mouse_pad(0003, "Mouse Pad", "Lutier");
 
     display_product(mouse_pad);
 
