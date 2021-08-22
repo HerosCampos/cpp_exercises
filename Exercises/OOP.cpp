33ab8672ea7f18e712465147c37898001fd79e5b
CodeCoin <herosrac@gmail.com>
2021-08-22 17:29:22 -0300

    Adding constructors

--- Exercises/OOP.cpp
+++ Exercises/OOP.cpp
@@ -10,13 +10,20 @@ public:
     std::string company;
     int age;
 
+    Employee(std::string name_val = "No name", std::string company_val = "No company", int age_val = 0);
+
     void introduce_yourself()
     {
         std::cout << "Name: " << name << std::endl;
         std::cout << "Age: " << age << std::endl;
         std::cout << "Company: " << company << std::endl;
+        std::cout << "====================" << std::endl;
     }
 };
+
+Employee::Employee(std::string name_val, std::string company_val, int age_val)
+    :name{name_val}, company{company_val}, age{age_val} {
+}
 
 
 
@@ -27,4 +34,10 @@ int main()
     frank.age = 61;
     frank.company = "ASDF";
     frank.introduce_yourself();
+
+    Employee james;
+    james.name = "James";
+    james.age = 77;
+    james.company = "FDSA";
+    james.introduce_yourself();
 }
