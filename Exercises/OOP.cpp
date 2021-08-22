278ec54e953d88da0182b7c050abf42c35c3b7bd
CodeCoin <herosrac@gmail.com>
2021-08-22 17:56:27 -0300

    Adding Copy Constructor and Destructor

--- Exercises/OOP.cpp
+++ Exercises/OOP.cpp
@@ -10,8 +10,6 @@ public:
     std::string company;
     int age;
 
-    Employee(std::string name_val = "No name", std::string company_val = "No company", int age_val = 0);
-
     void introduce_yourself()
     {
         std::cout << "Name: " << name << std::endl;
@@ -19,11 +17,26 @@ public:
         std::cout << "Company: " << company << std::endl;
         std::cout << "====================" << std::endl;
     }
+
+
+    Employee(std::string name_val = "No name", std::string company_val = "No company", int age_val = 0);
+    Employee(const Employee &source);
+    ~Employee()
 };
+
 
 Employee::Employee(std::string name_val, std::string company_val, int age_val)
     :name{name_val}, company{company_val}, age{age_val} {
 }
+
+// Employee::Employee(const Employee &source)
+//     :name{source.name}, age{source.age}, company{source.company} {
+// }
+
+Employee::Employee(const Employee &source)
+    :Employee{source.name, source.company, source.age} {
+}
+
 
 
 
