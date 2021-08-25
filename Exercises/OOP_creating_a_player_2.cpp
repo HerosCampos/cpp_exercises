05998c47f70b0f747728f03b0ad3fc80668b3df5
CodeCoin <herosrac@gmail.com>
2021-08-24 22:54:15 -0300

    OOP Player with Inheritance

--- Exercises/OOP_creating_a_player_2.cpp
+++ Exercises/OOP_creating_a_player_2.cpp
@@ -50,12 +50,61 @@ void display_player(Player p)
 // ========================================================================================
 
 
+class Warrior: public Player
+{
+private:
+    int hit;
+    int run;
+
+public:
+    int get_hit() {return hit;}
+    int get_run() {return run;}
+
+    void set_hit(int hit_val) {hit = hit_val;}
+    void set_run(int run_val) {run = run_val;}
+
+    Warrior(std::string name_val = "No name", int health_val = 0, int xp_val = 0, int hit_val = 0, int run_val = 0);
+
+    // void display_warrior(Warrior war)
+    // {
+    //     std::cout << "=============================" << std::endl;
+    //     std::cout << "Name: " << war.get_name() << std::endl;
+    //     std::cout << "Health: " << war.get_health() << std::endl;
+    //     std::cout << "XP: " << war.get_xp() << std::endl;
+    //     std::cout << "Hit: " << war.get_hit() << std::endl;
+    //     std::cout << "Run: " << war.get_run() << std::endl;
+    //     std::cout << "=============================" << std::endl;
+    // }
+};
+
+Warrior::Warrior(std::string name_val, int health_val, int xp_val, int hit_val, int run_val)
+    :Player(name_val, health_val, xp_val) 
+{
+    hit = hit_val, run = run_val;
+}
+
+
+void display_warrior(Warrior war)
+{
+    std::cout << "=============================" << std::endl;
+    std::cout << "Name: " << war.get_name() << std::endl;
+    std::cout << "Health: " << war.get_health() << std::endl;
+    std::cout << "XP: " << war.get_xp() << std::endl;
+    std::cout << "Hit: " << war.get_hit() << std::endl;
+    std::cout << "Run: " << war.get_run() << std::endl;
+    std::cout << "=============================" << std::endl;
+}
+
 
 // ========================================================================================
 
 
 int main()
 {
-    Player hero("Heros", 100, 87);
-    display_player(hero);
+    Warrior hero("Hero", 100, 93, 88, 54);
+    display_warrior(hero);
+
+    hero.set_xp(97);
+    hero.set_run(91);
+    display_warrior(hero);
 }
