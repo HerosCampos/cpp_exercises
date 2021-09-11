1cc530b8883c74348f0d39da1b91372757a0d36f
CodeCoin <herosrac@gmail.com>
2021-09-11 12:45:54 -0300

    Small Updates

--- QT/2/zoo/main.cpp
+++ QT/2/zoo/main.cpp
@@ -8,7 +8,11 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     Canine dog;
-    Feline cat;
+    dog.bark();
+    qInfo() << "Does the dog have BackBone:" << dog.hasBackBone();
+    qInfo() << "Is the dog alive:" << dog.isAlive();
+
+    // Feline cat;
 
     return a.exec();
 }
