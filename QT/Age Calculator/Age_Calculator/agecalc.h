33a77f9f956deea0ceede6b12551b26b952b4d08
CodeCoin <herosrac@gmail.com>
2021-09-12 17:21:05 -0300

    Age Calculator exercise finished

--- QT/Age Calculator/Age_Calculator/agecalc.h
+++ QT/Age Calculator/Age_Calculator/agecalc.h
@@ -2,12 +2,30 @@
 #define AGECALC_H
 
 #include <QObject>
+#include <QDebug>
+
 
 class AgeCalc : public QObject
 {
+private:
+    int m_age;
+    QString m_name;
+
     Q_OBJECT
 public:
     explicit AgeCalc(QObject *parent = nullptr);
+
+    int get_age() {return m_age;}
+    void set_age(int age) {m_age = age;}
+
+    QString get_name() {return m_name;}
+    void set_name(QString name) {m_name = name;}
+
+
+    int dogYears() const;
+    int catYears() const;
+    int humanYears() const;
+
 
 signals:
 
--- QT/Age Calculator/Age_Calculator/main.cpp
+++ QT/Age Calculator/Age_Calculator/main.cpp
@@ -1,8 +1,25 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include "agecalc.h"
+
+
+void display_ages(AgeCalc &calc)
+{
+    qInfo() << "Name:" << calc.get_name() << "is" << calc.get_age() << "years old.";
+    qInfo() << "Dog Age:" << calc.dogYears();
+    qInfo() << "Cat Age:" << calc.catYears();
+}
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    AgeCalc frank;
+    frank.set_age(45);
+    frank.set_name("Frank");
+
+    display_ages(frank);
 
     return a.exec();
 }
