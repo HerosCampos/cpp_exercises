bdd45dda15ffce5ce4803a15a6d7da43a0502879
CodeCoin <herosrac@gmail.com>
2021-09-23 22:36:33 -0300

    Personal_info class - DONE

--- Exercises/OOP_bank_account.cpp
+++ Exercises/OOP_bank_account.cpp
@@ -31,8 +31,41 @@ public:
 };
 
 
+Personal_info::Personal_info(std::string person_name_var, std::string person_job_title_var, double person_anual_wage_var, int person_id_var)
+    :person_name{person_name_var}, person_job_title{person_job_title_var}, person_anual_wage{person_anual_wage_var}, person_id{person_id_var} {
+        std::cout << "Constructor created for: " << person_name << std::endl;
+}
+
+Personal_info::Personal_info(const Personal_info &source)
+    :Personal_info(source.person_name, source.person_job_title, source.person_anual_wage, source.person_id) {
+        std::cout << "Copy constructor called for: " << source.person_name << std::endl;
+}
+
+
+void display_personal_info(Personal_info pi)
+{
+    std::cout << "Name: " << pi.get_person_name() << std::endl;
+    std::cout << "Job title: " << pi.get_person_job_title() << std::endl;
+    std::cout << "Anual wage: " << pi.get_person_anual_wage() << std::endl;
+    std::cout << "ID: " << pi.get_person_id() << std::endl;
+}
+
+
+// ===========================================================================================================
+
+
+
+
+// ===========================================================================================================
+
 
 int main()
 {
+    Personal_info heros;
+    heros.set_person_name("Heros");
+    heros.set_person_job_title("C++ Developer");
+    heros.set_person_anual_wage(90000);
+    heros.set_person_id(123456);
 
+    display_personal_info(heros);
 }
