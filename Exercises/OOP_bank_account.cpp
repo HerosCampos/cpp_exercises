7d4197283fdc4ccf69223ae1949deb9c7b2764e4
CodeCoin <herosrac@gmail.com>
2021-09-23 21:53:43 -0300

    Creating a bank account with OOP

---
+++ Exercises/OOP_bank_account.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class Personal_info
+{
+private:
+    std::string person_name;
+    std::string person_job_title;
+    double person_anual_wage;
+    int person_id;
+
+public:
+    
+};
+
+
+
+int main()
+{
+
+}
