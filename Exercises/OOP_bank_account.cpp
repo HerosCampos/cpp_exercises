1fa9056830310defca93645ce8276a6cb1975f40
CodeCoin <herosrac@gmail.com>
2021-09-29 22:31:56 -0300

    adding the Accounts class

--- Exercises/OOP_bank_account.cpp
+++ Exercises/OOP_bank_account.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 
-class Personal_info
+class Personal_Account
 {
 private:
     std::string person_name;
@@ -25,9 +25,9 @@ public:
     void set_person_id(int person_id_var) {person_id = person_id_var;}
     void set_balance(double balance_var) {balance = balance_var;}
 
-    Personal_info(std::string person_name_var = "No name", std::string person_job_title_var = "Unemployed", double person_anual_wage_var = 0.0, int person_id_var = 0, double balance_var = 0);
-    Personal_info(const Personal_info &source);
-    ~Personal_info()
+    Personal_Account(std::string person_name_var = "No name", std::string person_job_title_var = "Unemployed", double person_anual_wage_var = 0.0, int person_id_var = 0, double balance_var = 0);
+    Personal_Account(const Personal_Account &source);
+    ~Personal_Account()
     {
         std::cout << "Destructor called for: " << person_name << std::endl;
     }
@@ -37,18 +37,18 @@ public:
 };
 
 
-Personal_info::Personal_info(std::string person_name_var, std::string person_job_title_var, double person_anual_wage_var, int person_id_var, double balance_var)
+Personal_Account::Personal_Account(std::string person_name_var, std::string person_job_title_var, double person_anual_wage_var, int person_id_var, double balance_var)
     :person_name{person_name_var}, person_job_title{person_job_title_var}, person_anual_wage{person_anual_wage_var}, person_id{person_id_var}, balance{balance_var} {
         std::cout << "Constructor created for: " << person_name << std::endl;
 }
 
-Personal_info::Personal_info(const Personal_info &source)
-    :Personal_info(source.person_name, source.person_job_title, source.person_anual_wage, source.person_id, source.balance) {
+Personal_Account::Personal_Account(const Personal_Account &source)
+    :Personal_Account(source.person_name, source.person_job_title, source.person_anual_wage, source.person_id, source.balance) {
         std::cout << "Copy constructor called for: " << source.person_name << std::endl;
 }
 
 
-void display_personal_info(Personal_info pi)
+void display_personal_Account(Personal_Account pi)
 {
     std::cout << "Name: " << pi.get_person_name() << std::endl;
     std::cout << "Job title: " << pi.get_person_job_title() << std::endl;
@@ -58,14 +58,14 @@ void display_personal_info(Personal_info pi)
 }
 
 
-bool Personal_info::deposit(double amount)
+bool Personal_Account::deposit(double amount)
 {
     balance += amount;
     std::cout << "Deposit ok" << std::endl;
     return true;
 }
 
-bool Personal_info::withdraw(double amount)
+bool Personal_Account::withdraw(double amount)
 {
     if((balance - amount) >= 0)
     {
@@ -84,15 +84,70 @@ bool Personal_info::withdraw(double amount)
 // ===========================================================================================================
 
 
+class Accounts
+{
+private:
+    std::vector<Personal_Account> all_accounts;
+
+public:
+    bool add_account(std::string person_name_var, std::string person_job_title_var, double person_anual_wage_var, int person_id_var, double balance_var);
+    void display_accounts() const;
+};
+
+
+bool Accounts::add_account(std::string person_name_var, std::string person_job_title_var, double person_anual_wage_var, int person_id_var, double balance_var)
+{
+    for(Personal_Account person: all_accounts)
+    {
+        if(person.get_person_id() == person_id_var)
+        {
+            return false;
+        }
+    }
+    Personal_Account temp{person_name_var, person_job_title_var, person_anual_wage_var, person_id_var, balance_var};
+    all_accounts.push_back(temp);
+    return true;
+}
+
+
+void Accounts::display_accounts() const
+{
+    if(all_accounts.size() == 0)
+    {
+        std::cout << "No accounts to display!" << std::endl;
+    }
+    else
+    {
+        std::cout << "===============================================" << std::endl;
+        for(Personal_Account person: all_accounts)
+        {
+            std::cout << "Name: " << person.get_person_name() << std::endl;
+            std::cout << "Person ID: " << person.get_person_id() << std::endl;
+            std::cout << "Job title: " << person.get_person_job_title() << std::endl;
+            std::cout << "Salary (): " << person.get_person_anual_wage() << std::endl;
+
+        }
+        std::cout << "===============================================" << std::endl;
+    }
+}
+
+
+    // std::string person_job_title
+    // double person_anual_wage
+
+    // double balance
+// ===========================================================================================================
+
+
 int main()
 {
-    Personal_info heros;
+    Personal_Account heros;
     heros.set_person_name("Heros");
     heros.set_person_job_title("C++ Developer");
     heros.set_person_anual_wage(90000);
     heros.set_person_id(123456);
 
-    display_personal_info(heros);
+    display_personal_Account(heros);
 
     heros.set_balance(195888.69);
     heros.deposit(1000000);
@@ -111,7 +166,7 @@ int main()
     //         std::cout << "Insert the name of the account: " << std::endl;                      
     //         std::cin >> account_name;
 
-    //         Personal_info account_name;
+    //         Personal_Account account_name;
 
     //     }
     //     else if(opt == 2)
