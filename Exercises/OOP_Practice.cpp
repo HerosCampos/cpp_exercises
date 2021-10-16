0a290b5a4541de4a1c0b1d3cae0dfc5e535a669b
HerosCampos <herosrac@gmail.com>
2021-10-16 17:20:24 -0300

    Practicing OOP with C++

--- Exercises/OOP_Practice.cpp
+++ Exercises/OOP_Practice.cpp
@@ -5,10 +5,29 @@
 
 class Employee
 {
-public:
+private:
     std::string name;
     std::string company;
     int age;
+
+public:
+    std::string get_name() {return name;}
+    std::string get_company() {return company;}
+    int get_age() {return age;}
+
+    void set_name(std::string name_var) {name = name_var;}
+    void set_company(std::string company_var) {company = company_var;}
+    void set_age(int age_var) {age = age_var;}
+
+    // Constructor
+    Employee(std::string name_var = "no name", std::string company_var = "no company", int age_var = 0);
+    // Copy constructor
+    Employee(const Employee &source);
+    // Destructor
+    ~Employee()
+    {
+        std::cout << "Destructor called for: " << name << std::endl;
+    }
 
     void greeting()
     {
@@ -16,17 +35,27 @@ public:
     }
 };
 
+Employee::Employee(std::string name_var, std::string company_var, int age_var)
+    :name{name_var}, company{company_var}, age{age_var} {
+        std::cout << "Constructor called for: " << name << std::endl;
+}
+
+Employee::Employee(const Employee &source)
+    :Employee(source.name, source.company, source.age) {
+        std::cout << "Copy constructor called for: " << source.name << std::endl;
+}
+
 
 int main()
 {
     Employee James;
-    James.name = "James";
-    James.company = "Microsoft";
-    James.age = 56;
+    James.set_name("James");
+    James.set_company("Microsoft");
+    James.set_age(56);
 
-    std::cout << James.name << std::endl;
-    std::cout << James.company << std::endl;
-    std::cout << James.age << std::endl;
+    std::cout << James.get_name() << std::endl;
+    std::cout << James.get_company() << std::endl;
+    std::cout << James.get_age() << std::endl;
 
     James.greeting();
 }
