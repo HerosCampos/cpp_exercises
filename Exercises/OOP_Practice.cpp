7506cca8599d72f0bdd16c39d1ecc7a4a986b688
HerosCampos <herosrac@gmail.com>
2021-10-16 16:26:54 -0300

    Practicing OOP with C++

--- Exercises/OOP_Practice.cpp
+++ Exercises/OOP_Practice.cpp
@@ -3,7 +3,30 @@
 #include <string>
 
 
+class Employee
+{
+public:
+    std::string name;
+    std::string company;
+    int age;
+
+    void greeting()
+    {
+        std::cout << "Hello, my name is " << name << " I am " << age << " years old and I work for " << company << std::endl;
+    }
+};
+
+
 int main()
 {
-    
+    Employee James;
+    James.name = "James";
+    James.company = "Microsoft";
+    James.age = 56;
+
+    std::cout << James.name << std::endl;
+    std::cout << James.company << std::endl;
+    std::cout << James.age << std::endl;
+
+    James.greeting();
 }
