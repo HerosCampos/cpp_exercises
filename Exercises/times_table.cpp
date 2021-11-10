fd4835b834adc8687bc8a56c50b9558127dd8037
HerosCampos <herosrac@gmail.com>
2021-11-10 19:34:38 -0300

    Times table exercise with C++

---
+++ Exercises/times_table.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main()
+{
+    while(true)
+    {
+        std::cout << "Digite um número para ver sua tabuada: ";
+        int num;
+        std::cin >> num;
+
+        if(num != 0)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                std::cout << num << " x " << i << " = " << num * i << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Obrigado!" << std::endl;
+            break;
+        }
+    }
+}
