fc3faa97c2202bf723335c4bbf3f16862531fbe7
Heros <herosrac@gmail.com>
2022-04-18 21:44:48 -0300

    Back to the Basics - Learning about ternary operators

---
+++ Exercises/ternary_operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // std::cout << "Enter an integer: ";
+    // int num{};
+    // std::cin >> num;
+
+    // std::cout << num << " is " << ((num % 2 == 0) ? "even" : "odd") << std::endl;
+
+    int num1{50}, num2{75};
+
+    std::cout << num1 << " is " << ((num1 > num2) ? "bigger" : "smaller") << " than " << num2 << std::endl;
+
+}
