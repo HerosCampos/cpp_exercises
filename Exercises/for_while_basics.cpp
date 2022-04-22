9ab1e6d88eca23985f78152a7f70c20afaf274b4
Heros <herosrac@gmail.com>
2022-04-21 21:42:46 -0300

    Back to the basics - Small exercise about for and while loops

---
+++ Exercises/for_while_basics.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     if(i % 2 == 0) {
+    //         std::cout << i << " is even." << std::endl;
+    //     }
+    //     else {
+    //         std::cout << i << " is odd." << std::endl;
+    //     }
+    // }
+
+    // for(int i = 0; i <= 100; i++) {
+    //     std::cout << i << ((i % 10 == 0) ? "\n" : " ");
+    // }
+
+    // std::vector<int> nums {10, 20, 30, 40, 50};
+    // for(int i = 0; i < nums.size(); i++) {
+    //     std::cout << nums[i] << std::endl;
+    // }
+
+    // std::vector<int> nums {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    // for(int i : nums) {
+    //     if(i % 2 == 0) {
+    //         std::cout << i << " is even." << std::endl;
+    //     }
+    //     else {
+    //         std::cout << i << " is odd." << std::endl;
+    //     }
+    // }
+
+    // for(auto l: "Heros") {
+    //     if(l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u') {
+    //         std::cout << l << " is a vowel" << std::endl;
+    //     }
+    //     else {
+    //         std::cout << l << " is a consonant" << std::endl;
+    //     }
+    // }
+
+    // int number{100};
+    // while(number >= 100) {
+    //     std::cout << "Enter a number less than 100: ";
+    //     std::cin >> number;
+    // }
+
+    // int num{};
+    // while(true) {
+    //     std::cout << "Enter a number between 1 and 5: ";
+    //     std::cin >> num;
+    //     if(num < 1 || num > 5) {
+    //         std::cout << "Out of range, try again (1 to 5): ";
+    //         std::cin >> num;
+    //     }
+    //     else {
+    //         std::cout << "Thanks!" << std::endl;
+    //         break;
+    //     }
+    // }
+
+    
+}
